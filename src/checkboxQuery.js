fa12cd1923fb0686bd25b1782a43651c0f77b193
root <root@localhost.localdomain>
2018-03-23 19:49:24 +0530

    19:49

--- src/checkboxQuery.js
+++ src/checkboxQuery.js
@@ -1,15 +1,17 @@
 function checkFluency()
 {
     $('input[name=checkbox_1]').prop('checked', true);
+    $("#button_exportButton").show();
     $( ".oddhoverable" ).css( "background-color", "#FEFBCC" );
     $( ".evenhoverable" ).css( "background-color", "#FEFBCC" );
-    
+
 
 }
 $(document).ready(function(){
 $('#checkboxAll').change(function() {
 
 if(this.checked != true){
+  $("#button_exportButton").hide();
     $('input[name=checkbox_1]').prop('checked', false);
     $( ".oddhoverable" ).css( "background-color", "white" );
     $( ".evenhoverable" ).css( "background-color", "white" );
