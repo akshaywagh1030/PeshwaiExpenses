42a2b374637844e02481a0a702b51e619c37dd8c
root <root@localhost.localdomain>
2018-03-26 11:00:46 +0530

    11:00

--- src/checkboxQuery.js
+++ src/checkboxQuery.js
@@ -1,21 +1,58 @@
-function checkFluency()
-{
-    $('input[name=checkbox_1]').prop('checked', true);
-    $("#button_exportButton").show();
-    $( ".oddhoverable" ).css( "background-color", "#FEFBCC" );
-    $( ".evenhoverable" ).css( "background-color", "#FEFBCC" );
+$(document).ready(function() {
+  $('#checkboxAll').change(function() {
 
+    if (this.checked != true) {
+      $("#button_exportButton").hide();
+      $('input[name=checkbox_1]').prop('checked', false);
+      $(".oddhoverable").css("background-color", "white");
+      $(".evenhoverable").css("background-color", "white");
 
-}
-$(document).ready(function(){
-$('#checkboxAll').change(function() {
+    } else {
+      $('input[name=checkbox_1]').prop('checked', true);
+      $("#button_exportButton").show();
+      $(".oddhoverable").css("background-color", "#FEFBCC");
+      $(".evenhoverable").css("background-color", "#FEFBCC");
+    }
+  });
+  $('input[name=checkbox_1]').change(function() {
 
-if(this.checked != true){
-  $("#button_exportButton").hide();
-    $('input[name=checkbox_1]').prop('checked', false);
-    $( ".oddhoverable" ).css( "background-color", "white" );
-    $( ".evenhoverable" ).css( "background-color", "white" );
+    if (this.checked != true)
+    {
+      $("#reportsTable_29394607").css("background-color", "white");
 
-}
-});
+    } else {
+      $("#reportsTable_29394607").css("background-color", "#FEFBCC");
+    }
+  });
+
+  $('input[name=checkbox_2]').change(function() {
+
+    if (this.checked != true)
+    {
+      $("#reportsTable_29332021").css("background-color", "white");
+
+    } else {
+      $("#reportsTable_29332021").css("background-color", "#FEFBCC");
+    }
+  });
+  $('input[name=checkbox_3]').change(function() {
+
+    if (this.checked != true)
+    {
+      $("#reportsTable_29331719").css("background-color", "white");
+
+    } else {
+      $("#reportsTable_29331719").css("background-color", "#FEFBCC");
+    }
+  });
+  $('input[name=checkbox_4]').change(function() {
+
+    if (this.checked != true)
+    {
+      $("#reportsTable_29331658").css("background-color", "white");
+
+    } else {
+      $("#reportsTable_29331658").css("background-color", "#FEFBCC");
+    }
+  });
 });
